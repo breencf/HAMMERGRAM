f515a8c62f54038acdd9ecf6505ba8ca78417cfd
Chris Breen <breencf@gmail.com>
2022-03-14 19:55:32 -0400

    signup routes and validations

---
+++ backend/routes/api/session.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
+const { setTokenCookie, restoreUser } = require("../../utils/auth");
+const { User } = require("../../db/models");
+
+const router = express.Router();
+
+const validateLogin = [
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email address"),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a valid password"),
+  handleValidationErrors,
+];
+//login
+router.post(
+  "/",
+  validateLogin,
+  asyncHandler(async (req, res, next) => {
+    const { credential, password } = req.body;
+
+    const user = await User.login(credential, password);
+
+    if (!user) {
+      const e = new Error("Login Failed");
+      e.status = 401;
+      e.title = "Login Failed";
+      e.errors = ["The provided credentials were invalid"];
+      return next(e);
+    }
+
+    await setTokenCookie(res, user);
+    return res.json({ user });
+  })
+);
+//logout
+router.delete(
+  "/",
+  asyncHandler(async (_req, res) => {
+    res.clearCookie("token");
+    return res.json({ message: "success" });
+  })
+);
+
+router.get(
+  "/",
+  restoreUser,
+  asyncHandler(async (req, res) => {
+    const { user } = req;
+    if (user) {
+      return res.json({
+        user: user.toSafeObject(),
+      });
+    } else return res.json({});
+  })
+);
+
+module.exports = router;
---
+++ backend/routes/api/users.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { User } = require("../../db/models");
+
+
+const router = express.Router();
+
+const validateSignup = [
+    check("email")
+      .exists({ checkFalsy: true })
+      .isEmail()
+      .withMessage("Please provide a valid email"),
+    check("name")
+      .exists({ checkFalsy: true })
+      .withMessage(
+        "A name is required."
+      ),
+    check("username")
+      .exists({ checkFalsy: true })
+      .isLength({ min: 3 })
+      .withMessage(
+        "Please provide a username that is a minimum of 3 characters and on in use"
+      ),
+    check("password")
+      .exists({ checkFalsy: true })
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+    handleValidationErrors,
+  ];
+
+router.post(
+  "/", validateSignup,
+  asyncHandler(async (req, res) => {
+    const { email, name, image, password, username } = req.body;
+    const user = await User.signup({ email, name, image, password, username });
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user,
+    });
+  })
+);
+
+module.exports = router;
