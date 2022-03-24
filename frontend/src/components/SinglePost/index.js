fbe83acdb14afaf4a4041f9cb4dd365306f81072
Chris Breen <breencf@gmail.com>
2022-03-24 15:22:58 -0400

    more padding

--- frontend/src/components/SinglePost/index.js
+++ frontend/src/components/SinglePost/index.js
@@ -66,79 +66,85 @@ export const PostPage = () => {
     <>
       {showEdit && <EditPost content={content} hideForm={setShowEdit} />}
       {!showEdit && (
-        <div className="post-container">
-          <div className="post-top">
-            <div className="post-top-left">
-              <img className="userIcon" src={content?.User?.image} />
-              <div className="post-top-user-loc">
-                <Link to={`/users/${content?.User?.id}`}>
-                  {content?.User?.username}
-                </Link>
-                {content?.location && <p>{content?.location}</p>}
+        <div className="single-post-buffer">
+          <div className="post-container">
+            <div className="post-top">
+              <div className="post-top-left">
+                <img className="userIcon" src={content?.User?.image} />
+                <div className="post-top-user-loc">
+                  <Link to={`/users/${content?.User?.id}`}>
+                    {content?.User?.username}
+                  </Link>
+                  {content?.location && <p>{content?.location}</p>}
+                </div>
+              </div>
+              <button className="button-none" onClick={() => openModal()}>
+                <FaEllipsisH />
+              </button>
+            </div>
+            <div className="post-image">
+              <img src={content?.image} {...bind} onDoubleClick={onDClick} />
+              {showHeart && (
+                <div className="bigHeart">
+                  <FaHeart />
+                </div>
+              )}
+            </div>
+            <div className="post-bottom">
+              <div className="post-bottom-top">
+                <div className="post-bottom-top-left">
+                  {content && <LikeButton postId={content?.id} />}
+                  <Link to={`/posts/${content?.id}/comments`}>
+                    <FaRegCommentDots />
+                  </Link>{" "}
+                  <FaRegPaperPlane />
+                </div>
+                <FaRegBookmark />
+              </div>
+              <div className="post-bottom-bottom">
+                <div>
+                  <Link to={`/posts/${content?.id}/likes`}>
+                    {likeCount} likes
+                  </Link>
+                </div>
+                <div>
+                  <Link to={`/users/${content?.User?.username}`}>
+                    {content?.User?.username}
+                  </Link>
+                  {content?.caption}
+                </div>
+                <div className="comments">
+                  {content?.Comments?.map((comment) => {
+                    return (
+                      comment && (
+                        <div key={comment.id}>
+                          <Link to={`/users/${comment.userId}`}>
+                            {comment?.User?.username}
+                          </Link>{" "}
+                          {comment?.content}
+                        </div>
+                      )
+                    );
+                  })}
+                </div>
               </div>
             </div>
-            <button className="button-none" onClick={() => openModal()}>
-              <FaEllipsisH />
-            </button>
+            <Modal
+              isOpen={modalIsOpen}
+              onRequestClose={closeModal}
+              style={modalStyle}
+              ariaHideApp={false}
+            >
+              {content && (
+                <OptionsMenu
+                  content={content}
+                  singlePost={true}
+                  closeModal={closeModal}
+                  setShowEdit={setShowEdit}
+                />
+              )}
+            </Modal>
           </div>
-          <div className="post-image">
-            <img src={content?.image} {...bind} onDoubleClick={onDClick} />
-            {showHeart && (
-              <div className="bigHeart">
-                <FaHeart />
-              </div>
-            )}
-          </div>
-          <div className="post-bottom">
-            <div className="post-bottom-top">
-              <div className="post-bottom-top-left">
-                {content && <LikeButton postId={content?.id} />}
-                <Link to={`/posts/${content?.id}/comments`}>
-                  <FaRegCommentDots />
-                </Link>{" "}
-                <FaRegPaperPlane />
-              </div>
-              <FaRegBookmark />
-            </div>
-            <div className="post-bottom-bottom">
-              <div><Link to={`/posts/${content?.id}/likes`}>{likeCount} likes</Link></div>
-              <div>
-                <Link to={`/users/${content?.User?.username}`}>
-                  {content?.User?.username}
-                </Link>
-                {content?.caption}
-              </div>
-              <div className="comments">
-                {content?.Comments?.map((comment) => {
-                  return (
-                    comment && (
-                      <div key={comment.id}>
-                        <Link to={`/users/${comment.userId}`}>
-                          {comment?.User?.username}
-                        </Link>{" "}
-                        {comment?.content}
-                      </div>
-                    )
-                  );
-                })}
-              </div>
-            </div>
-          </div>
-          <Modal
-            isOpen={modalIsOpen}
-            onRequestClose={closeModal}
-            style={modalStyle}
-            ariaHideApp={false}
-          >
-            {content && (
-              <OptionsMenu
-                content={content}
-                singlePost={true}
-                closeModal={closeModal}
-                setShowEdit={setShowEdit}
-              />
-            )}
-          </Modal>
         </div>
       )}
     </>
